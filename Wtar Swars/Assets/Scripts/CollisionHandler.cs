731fce539bf09f233082276da3fdc994dab00876
Emil Ostrowski <emil090596@gmail.com>
2022-12-12 02:35:47 +0100

    Enemy explosion, HP and UI
    Adding enemy explosion, HP and score UI

--- Wtar Swars/Assets/Scripts/CollisionHandler.cs
+++ Wtar Swars/Assets/Scripts/CollisionHandler.cs
@@ -5,24 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    [SerializeField] float loadDelay = 1f;
+    [SerializeField] float loadDelay = 2f;
     [SerializeField] ParticleSystem crashVFX;
 
     void OnTriggerEnter(Collider other)
     {
         StartCrashSequence();
-        Invoke("VanishShip", 0.5f);
+        //using VanishShip method after 1 sec
+        Invoke("VanishShip", 1f);
     }
 
     void StartCrashSequence()
     {
+        //playing crasfVFX and disabling playercontrol script
         crashVFX.Play();
         GetComponent<PlayerControl>().enabled = false;
+        //restarting level after 2sec
         Invoke("ReloadLevel", loadDelay);
     }
 
     void VanishShip()
     {
+        //vanishing ship (for all mesh rendere of children)
         foreach (Renderer r in GetComponentsInChildren<Renderer>())
             r.enabled = false;
     }
--- Wtar Swars/Assets/Scripts/Enemy.cs
+++ Wtar Swars/Assets/Scripts/Enemy.cs
@@ -4,8 +4,43 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private void OnParticleCollision(GameObject other)
+    [SerializeField] GameObject destroyVFX;
+    [SerializeField] GameObject hitVFX;
+    [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 15;
+    [SerializeField] int hitPoints = 5;
+
+
+    ScoreBoard scoreBorad;
+
+    void Start()
     {
+        scoreBorad = FindObjectOfType<ScoreBoard>();    
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        //killing enemy after HP go to 0
+        if (hitPoints < 1)
+        {
+            KillEnemy();
+        }
+    }
+
+    void ProcessHit()
+    {
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
+        // -1 hp after hit
+        hitPoints = hitPoints - 1;
+        scoreBorad.IncreaseScore(scorePerHit);
+    }
+
+    void KillEnemy()
+    {
+        GameObject vfx = Instantiate(destroyVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
         Destroy(gameObject);
     }
 }
